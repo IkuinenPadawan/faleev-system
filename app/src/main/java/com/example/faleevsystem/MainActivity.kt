1d0cac0a17edaa27124b90569582f942225533ba
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2021-05-12 12:18:20 +0300

    Initial commit of Mars Weather App.
    Faleev System app scrapped. Currently fetching weather data works
    and is shown in log.

--- app/src/main/java/com/example/faleevsystem/MainActivity.kt
+++ app/src/main/java/com/example/faleevsystem/MainActivity.kt
@@ -5,43 +5,43 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import android.widget.*
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var listView : ListView
-    lateinit var adapter: ArrayAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        title = "Mars Weather"
 
-        Log.d("TAG", "STARTING")
-        listView = findViewById(R.id.listView)
-
-        var arr: Array<String> = arrayOf("green", "red", "blue")
-
-        adapter = ArrayAdapter(this, R.layout.row, R.id.day, arr)
-        listView.adapter = adapter
-
-        // list view item click listener
-        listView.setOnItemClickListener { parent, view, position, id ->
-            //val element = adapter.getItemAtPosition(position) // The item that was clicked
-            val intent = Intent(this, WorkoutActivity::class.java)
-            Log.d("TAG", "Click")
-            startActivity(intent)
+        downloadUrlAsync(this, "https://api.nasa.gov/insight_weather/?api_key=DEMO_KEY&feedtype=json&ver=1.0") {
+            println(it)
+            Log.d("Tag", it)
         }
 
-        /*listView.onItemClickListener = AdapterView.OnItemClickListener { parent, view, position, id ->
-            val selectedItemText = parent.getItemAtPosition(position)
-        }*/
-
-    }
-
-    fun openWorkout(view: Context) {
-        val intent = Intent(this, WorkoutActivity::class.java)
-        startActivityForResult(intent, 12)
     }
 }
+
+private fun downloadUrlAsync(context: Context, url: String, cb: (String) -> Unit) {
+    thread() {
+        val myUrl = URL(url)
+        val conn = myUrl.openConnection() as HttpURLConnection
+
+        var result = ""
+
+        var reader = BufferedReader(InputStreamReader(conn.inputStream))
+
+        val allText = conn.inputStream.bufferedReader().use(BufferedReader::readText)
+
+        cb(allText)
+    }
+}
+
+
+
---
+++ app/src/main/java/com/example/faleevsystem/MyData.kt
@@ -0,0 +1,8 @@
+package com.example.faleevsystem
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class Weather() {
+
+}
